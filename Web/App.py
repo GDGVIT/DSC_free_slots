82b42c08a185c5a1e52e58e96a85a4c38991573e
paritoshM9 <paritosh.mahajan98@gmail.com>
2018-06-02 22:15:11 +0530

    upload screenshots on web

---
+++ Web/App.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+
+app = Flask(__name__)
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        return filename
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=8000,debug=True)
